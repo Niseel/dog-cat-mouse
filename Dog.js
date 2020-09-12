413290e1b5fa791005428ecb5a35116414a1a999
Niseel <nguyencongthanh200199@gmail.com>
2020-09-12 19:56:45 +0700

    Add nodejs: chalk in Dog.js module and Using in index.js

--- Dog.js
+++ Dog.js
@@ -1,7 +1,11 @@
+var chalk = require('chalk');
+
 function Dog(name) {
     this.name = name;    
 }
 
 Dog.prototype.bark = function(){
-    console.log('Gau Gau....');
+    console.log('Gau Gau....', 'My name is', chalk.red(this.name));
 }
+
+module.exports = Dog;
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var Cat = require('./Cat');
 var Mouse = require('./Mouse');
-
+var Dog = require('./Dog');
 var tom = new Cat('Tom', 5, 'Blue');
 
 console.log(tom);
 tom.run();
 
+console.log('New Friend');
+var dog = new Dog('Bub');
+dog.bark();
 
